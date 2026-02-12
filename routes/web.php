14c50e9cc0f9a8c0e2063ee8667d8c9fd8a0edbe
Jorge Luquetta <Jluquetta@unab.edu.co>
2026-02-12 11:21:37 -0500

    mi README

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mi-nombre', function () {
+    return "Jorge Andrés Luquetta Aguilera";
+});
+
+Route::get('/mi-nombre/php-basico', function () {
+    return "Jorge Andrés Luquetta Aguilera";
+});
